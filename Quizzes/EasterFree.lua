0f008404af89f77686670eca70d0f166de59a59c
Zak Boem <63625087+cosmixcom@users.noreply.github.com>
2026-04-10 10:20:49 -0700

    Add Easter Pack (Free) with questions and options

---
+++ Quizzes/EasterFree.lua
@@ -0,0 +1,62 @@
+-- Easter Pack (Free)
+-- Place in ReplicatedStorage/Packs/easter_free
+
+local pack = {
+	name = "Easter Pack (Free)",
+	type = "free",
+	Image = "73655562881156",
+	questions = {
+		{
+			questionTemplate = "What is {player}'s favorite Easter candy?",
+			personalQuestion = "What is your favorite Easter candy?",
+			options = {"Chocolate Eggs", "Jelly Beans", "Marshmallow Peeps", "Chocolate Bunny", "Caramel Eggs", "Candy Mix"}
+		},
+		{
+			questionTemplate = "What does {player} enjoy most on Easter?",
+			personalQuestion = "What do you enjoy most on Easter?",
+			options = {"Egg Hunts", "Eating Candy", "Family Time", "Decorating Eggs", "Relaxing", "Going Outside"}
+		},
+		{
+			questionTemplate = "What is {player}'s favorite Easter activity?",
+			personalQuestion = "What is your favorite Easter activity?",
+			options = {"Egg Hunt", "Egg Decorating", "Baking Treats", "Picnic", "Games", "Watching Movies"}
+		},
+		{
+			questionTemplate = "What color eggs does {player} like most?",
+			personalQuestion = "What color eggs do you like most?",
+			options = {"Pastel Colors", "Bright Colors", "Rainbow", "Gold", "Natural", "Mixed"}
+		},
+		{
+			questionTemplate = "What does {player} put in Easter baskets?",
+			personalQuestion = "What do you put in Easter baskets?",
+			options = {"Candy", "Toys", "Chocolate", "Gift Cards", "Stuffed Animals", "Surprises"}
+		},
+		{
+			questionTemplate = "What Easter food does {player} prefer?",
+			personalQuestion = "What Easter food do you prefer?",
+			options = {"Ham", "Roast Chicken", "Deviled Eggs", "Salad", "Bread", "Desserts"}
+		},
+		{
+			questionTemplate = "Where would {player} want to have an Easter egg hunt?",
+			personalQuestion = "Where would you want to have an Easter egg hunt?",
+			options = {"Backyard", "Park", "Beach", "Indoors", "Garden", "Anywhere Fun"}
+		},
+		{
+			questionTemplate = "What kind of Easter bunny does {player} like?",
+			personalQuestion = "What kind of Easter bunny do you like?",
+			options = {"Cute", "Funny", "Realistic", "Cartoon", "Giant", "None"}
+		},
+		{
+			questionTemplate = "What drink does {player} like with Easter treats?",
+			personalQuestion = "What drink do you like with Easter treats?",
+			options = {"Milk", "Juice", "Soda", "Tea", "Coffee", "Water"}
+		},
+		{
+			questionTemplate = "What outfit would {player} wear on Easter?",
+			personalQuestion = "What outfit would you wear on Easter?",
+			options = {"Casual", "Fancy", "Pastel Outfit", "Dress/Suit", "Comfortable", "Anything"}
+		}
+	}
+}
+
+return pack
